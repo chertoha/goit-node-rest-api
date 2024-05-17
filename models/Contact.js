1b614abd9ec23298371bb68bee031b8e19f0c8b0
Anton <a.chertok@akatech.com.ua>
2024-05-17 23:45:34 +0100

    feat: auth

--- models/Contact.js
+++ models/Contact.js
@@ -20,6 +20,11 @@ const contactSchema = new Schema(
       type: Boolean,
       default: false,
     },
+
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
   },
   { versionKey: false, timestamps: true }
 );
